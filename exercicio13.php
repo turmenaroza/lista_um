800462e7ceceaad376ce56713f276d7dcccb27b0
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-07-15 11:27:19 -0300

    add exercicio 13

---
+++ exercicio13.php
@@ -0,0 +1,21 @@
+
+<html>
+<head>
+    <title>celsius to fahrenheit Conversion</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="celsius">celsius:</label>
+        <input type="text" id="celsius" name="celsius">
+        <input type="submit" value="Convert">
+    </form>
+
+    <?php
+    if (isset($_POST['celsius'])) {
+        $celsius = $_POST['celsius'];
+        $fahrenheit = ($celsius *1.8) +32;
+        echo "<p>{$celsius}°C é {$fahrenheit}°F</p>";
+    }
+    ?>
+</body>
+</html>
